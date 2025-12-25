d95ac261a8aa33b69ac452d520e58aacec993b56
Lilian Mo <lilian@mxk8us.com>
2025-12-24 16:51:09 -0800

    update firebase

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXEtq0ubgtXIbb7s_JzoWt8daNejKwuLQ',
+  authDomain: 'mindmap-9f454.firebaseapp.com',
+  projectId: 'mindmap-9f454',
+  storageBucket: 'mindmap-9f454.firebasestorage.app',
+  messagingSenderId: '582191293462',
+  appId: '1:582191293462:web:b10a7ee6f2c776f5538137'
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
